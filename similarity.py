266d11c62eae6834d8ada26114c3a6a572c3e64c
tsitsidalakishvili <114109718+tsitsidalakishvili@users.noreply.github.com>
2023-09-29 10:17:10 +0100

    Update similarity.py

--- similarity.py
+++ similarity.py
@@ -8,47 +8,41 @@ import nltk
 # Download stopwords if not already downloaded
 nltk.download('stopwords')
 
-def preprocess_data(dataframe, Eigen=None):  # Add default value for Eigen if it's optional
-    # Preprocessing code here
+def preprocess_data(dataframe, text_column):
     stop_words = set(stopwords.words('english'))
-    dataframe['clean_text'] = dataframe['Summary'].apply(lambda x: ' '.join([word for word in str(x).lower().split() if word not in stop_words]))
+
+    # Ensure the text_column exists in the dataframe
+    if text_column not in dataframe.columns:
+        raise ValueError(f"{text_column} does not exist in the dataframe")
+
+    dataframe['clean_text'] = dataframe[text_column].apply(lambda x: ' '.join([word for word in str(x).lower().split() if word not in stop_words]))
     dataframe['clean_text'] = dataframe['clean_text'].apply(lambda x: re.sub(r'\b[\w\.-]+@[\w\.-]+\.\w+\b', '', x))
     dataframe['clean_text'] = dataframe['clean_text'].apply(lambda x: re.sub(r'http\S+|www\S+', '', x))
     dataframe['clean_text'] = dataframe['clean_text'].apply(lambda x: re.sub(r'<.*?>', '', x))
+    
     return dataframe
 
-def calculate_similarity(df, threshold):
-    # Initialize the TF-IDF vectorizer
+def calculate_similarity(df, threshold, identifier_column, text_column, additional_columns):
     vectorizer = TfidfVectorizer()
-
-    # Compute the TF-IDF matrix for the summaries
-    tfidf_matrix = vectorizer.fit_transform(df['clean_text'])
-
-    # Compute the cosine similarity matrix
+    tfidf_matrix = vectorizer.fit_transform(df[text_column])
     similarity_matrix = cosine_similarity(tfidf_matrix)
 
-    # Get the indices of summaries in ascending order of similarity from summary 0
-    summary_index = 0
-
-    # Create a list to store the pairs of nodes and their similarities
     pairs = []
-
-    # Iterate over the similarity matrix
     for i in range(len(similarity_matrix)):
         for j in range(i + 1, len(similarity_matrix)):
-            if similarity_matrix[i, j] > threshold:  # Set a threshold for similarity
-                summary1_id = df['Issue key'].iloc[i]
-                summary2_id = df['Issue key'].iloc[j]
+            if similarity_matrix[i, j] > threshold:
+                summary1_id = df[identifier_column].iloc[i]
+                summary2_id = df[identifier_column].iloc[j]
                 similarity = similarity_matrix[i, j]
-
-                # Append the pair of nodes and their similarity to the list
                 pairs.append((summary1_id, summary2_id, similarity))
 
-    # Convert the list of pairs to a DataFrame
     pairs_df = pd.DataFrame(pairs, columns=['Node 1', 'Node 2', 'Similarity'])
 
-    # Merge with original DataFrame to include Issue key, Summary, and Story Points columns
-    merged_df = pd.merge(pairs_df, df[['Issue key', 'Summary', 'Custom field (Story Points)']], left_on='Node 1', right_on='Issue key', how='left')
-    merged_df = pd.merge(merged_df, df[['Issue key', 'Summary', 'Custom field (Story Points)']], left_on='Node 2', right_on='Issue key', how='left', suffixes=('_Node1', '_Node2'))
+    if additional_columns:
+        for col in additional_columns:
+            temp_df1 = df[[identifier_column, col]].add_suffix(f'_Node1').rename(columns={f'{identifier_column}_Node1': 'Node 1'})
+            temp_df2 = df[[identifier_column, col]].add_suffix(f'_Node2').rename(columns={f'{identifier_column}_Node2': 'Node 2'})
+            pairs_df = pd.merge(pairs_df, temp_df1, on='Node 1', how='left')
+            pairs_df = pd.merge(pairs_df, temp_df2, on='Node 2', how='left')
 
-    return merged_df
+    return pairs_df
