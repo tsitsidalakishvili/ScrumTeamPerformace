ebd6869e2b61260f18c1721d77868a46f1ab9bfb
tsitsidalakishvili <114109718+tsitsidalakishvili@users.noreply.github.com>
2023-11-21 20:58:49 +0400

    Add files via upload

---
+++ 4_📄_chat_with_your_documents.py
@@ -0,0 +1,121 @@
+import os
+import utils
+import streamlit as st
+from streaming import StreamHandler
+import pandas as pd
+
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import PyPDFLoader
+from langchain.memory import ConversationBufferMemory
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.chains import ConversationalRetrievalChain
+from langchain.vectorstores import DocArrayInMemorySearch
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+st.set_page_config(page_title="ChatPDF", page_icon="📄")
+st.header('Chat with your documents')
+st.write('Has access to custom documents and can respond to user queries by referring to the content within those documents')
+
+
+
+class CustomDataChatbot:
+
+    def __init__(self):
+        utils.configure_openai_api_key()
+        self.openai_model = "gpt-3.5-turbo"
+
+    def save_file(self, file):
+        folder = 'tmp'
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        
+        file_path = f'./{folder}/{file.name}'
+        with open(file_path, 'wb') as f:
+            f.write(file.getvalue())
+        return file_path
+    
+    def save_csv_file(self, file):
+        folder = 'tmp'
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+        file_path = f'./{folder}/{file.name}'
+        with open(file_path, 'wb') as f:
+            f.write(file.getvalue())
+        return file_path
+
+    def process_csv_data(self, file_path):
+        # Read CSV file into a Pandas DataFrame
+        df = pd.read_csv(file_path)
+        # You can add additional processing steps based on your requirements
+        return df
+
+    @st.spinner('Analyzing documents..')
+    def setup_qa_chain(self, uploaded_files):
+        # Load documents
+        docs = []
+        for file in uploaded_files:
+            file_path = self.save_file(file)
+            loader = PyPDFLoader(file_path)
+            docs.extend(loader.load())
+        
+        # Split documents
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=500,
+            chunk_overlap=20
+        )
+        splits = text_splitter.split_documents(docs)
+         
+        # Check if there are any CSV files
+        csv_files = [file for file in uploaded_files if file.type == 'application/vnd.ms-excel']
+        for file in csv_files:
+            file_path = self.save_csv_file(file)
+            csv_data = self.process_csv_data(file_path)
+            # Add logic to handle CSV data as needed 
+
+
+        # Create embeddings and store in vectordb
+        embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+        vectordb = DocArrayInMemorySearch.from_documents(splits, embeddings)
+
+        # Define retriever
+        retriever = vectordb.as_retriever(
+            search_type='mmr',
+            search_kwargs={'k':2, 'fetch_k':4}
+        )
+
+        # Setup memory for contextual conversation        
+        memory = ConversationBufferMemory(
+            memory_key='chat_history',
+            return_messages=True
+        )
+
+        # Setup LLM and QA chain
+        llm = ChatOpenAI(model_name=self.openai_model, temperature=0.8, streaming=True)
+        qa_chain = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory, verbose=True)
+        return qa_chain
+
+    @utils.enable_chat_history
+    def main(self):
+
+        # User Inputs
+        uploaded_files = st.sidebar.file_uploader(label='Upload files', type=['pdf', 'csv'], accept_multiple_files=True)
+        if not uploaded_files:
+            st.error("Please upload PDF documents to continue!")
+            st.stop()
+
+        user_query = st.chat_input(placeholder="Ask me anything!")
+
+        if uploaded_files and user_query:
+            qa_chain = self.setup_qa_chain(uploaded_files)
+
+            utils.display_msg(user_query, 'user')
+
+            with st.chat_message("assistant"):
+                st_cb = StreamHandler(st.empty())
+                response = qa_chain.run(user_query, callbacks=[st_cb])
+                st.session_state.messages.append({"role": "assistant", "content": response})
+
+if __name__ == "__main__":
+    obj = CustomDataChatbot()
+    obj.main()
