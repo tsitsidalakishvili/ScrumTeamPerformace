2bd491bbbd3eab42039432a2c421e447b410e8ab
tsitsidalakishvili <114109718+tsitsidalakishvili@users.noreply.github.com>
2024-02-13 20:23:51 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1147,7 +1147,7 @@ def Similarity_Analysis(df):
             similarity_func(df)
 
     # Create expander for Information Box
-    with st.expander("How it works", expanded=False):
+    with st.sidebar("How it works", expanded=False):
         st.markdown("""
         The script is designed to process and analyze text data, identifying and quantifying similarities between different pieces of text.
 
@@ -1160,65 +1160,7 @@ def Similarity_Analysis(df):
         - *Calculating & Filtering Similarities*: The script calculates the similarity between each piece of text and filters out pairs that have a similarity score above a set threshold. The result is a list of text pairs deemed sufficiently similar, accompanied by their similarity scores and relevant information from the original data.
         """)
 
-    # Create expander for Script Box
-    with st.expander("Script", expanded=False):
-        st.code("""
-        import pandas as pd
-        from nltk.corpus import stopwords
-        import re
-        from sklearn.feature_extraction.text import TfidfVectorizer
-        from sklearn.metrics.pairwise import cosine_similarity
-        import nltk
-        
-        # Download stopwords if not already downloaded
-        nltk.download('stopwords')
-        
-        def preprocess_data(dataframe, Eigen=None):  # Add default value for Eigen if it's optional
-            # Preprocessing code here
-            stop_words = set(stopwords.words('english'))
-            dataframe['clean_text'] = dataframe['Summary'].apply(lambda x: ' '.join([word for word in str(x).lower().split() if word not in stop_words]))
-            dataframe['clean_text'] = dataframe['clean_text'].apply(lambda x: re.sub(r'\b[\w\.-]+@[\w\.-]+\.\w+\b', '', x))
-            dataframe['clean_text'] = dataframe['clean_text'].apply(lambda x: re.sub(r'http\S+|www\S+', '', x))
-            dataframe['clean_text'] = dataframe['clean_text'].apply(lambda x: re.sub(r'<.*?>', '', x))
-            return dataframe
-        
-        def calculate_similarity(df, threshold):
-            # Initialize the TF-IDF vectorizer
-            vectorizer = TfidfVectorizer()
-        
-            # Compute the TF-IDF matrix for the summaries
-            tfidf_matrix = vectorizer.fit_transform(df['clean_text'])
-        
-            # Compute the cosine similarity matrix
-            similarity_matrix = cosine_similarity(tfidf_matrix)
-        
-            summary_index = 0
-        
-            # Create a list to store the pairs of nodes and their similarities
-            pairs = []
-        
-            # Iterate over the similarity matrix
-            for i in range(len(similarity_matrix)):
-                for j in range(i + 1, len(similarity_matrix)):
-                    if similarity_matrix[i, j] > threshold:  # Set a threshold for similarity
-                        summary1_id = df['Issue key'].iloc[i]
-                        summary2_id = df['Issue key'].iloc[j]
-                        similarity = similarity_matrix[i, j]
-        
-                        # Append the pair of nodes and their similarity to the list
-                        pairs.append((summary1_id, summary2_id, similarity))
-        
-            # Convert the list of pairs to a DataFrame
-            pairs_df = pd.DataFrame(pairs, columns=['Node 1', 'Node 2', 'Similarity'])
-        
-            # Merge with original DataFrame to include Issue key, Summary, and Story Points columns
-            merged_df = pd.merge(pairs_df, df[['Issue key', 'Summary', 'Custom field (Story Points)']], left_on='Node 1', right_on='Issue key', how='left')
-            merged_df = pd.merge(merged_df, df[['Issue key', 'Summary', 'Custom field (Story Points)']], left_on='Node 2', right_on='Issue key', how='left', suffixes=('_Node1', '_Node2'))
-        
-            return merged_df
-        """, language='python')
-
-
+    
 
 
 #---------------------------------------------------------------------------------------------------#
